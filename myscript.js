444d25a53848be497e89b2f769498428c2bbd632
mleonard7 <97709822+mleonard7@users.noreply.github.com>
2022-03-31 19:22:55 -0400

    First JavaScript Update

--- myscript.js
+++ myscript.js
@@ -1,10 +1,38 @@
 $(document).ready(function (){
-    $("#readmore" ).click(function ()f
-    §(".hidden"). fadeIn();
-    $("#readmore" ). fadeout();
-    }):
-    $("#readless" ).click(function() {
-    $(".hidden"). fadeOut();
-    §("#readmore"). fadeIn();
+    $("#readmore-nearpod").click(function() {
+    $("#nearpod"). fadeIn();
+    $("#readmore-nearpod"). fadeOut();
     });
+    $("#readless-nearpod").click(function() {
+    $("#nearpod"). fadeOut();
+    $("#readmore-nearpod"). fadeIn();
+    });
+
+    $("#readmore-explaineverything").click(function() {
+    $("#explaineverything"). fadeIn();
+    $("#readmore-explaineverything"). fadeOut();
+    });
+    $("#readless-explaineverything").click(function() {
+    $("#explaineverything"). fadeOut();
+    $("#readmore-explaineverything"). fadeIn();
+    });
+
+    $("#readmore-generationgenius").click(function() {
+    $("#generationgenius"). fadeIn();
+    $("#readmore-generationgenius"). fadeOut();
+    });
+    $("#readless-generationgenius").click(function() {
+    $("#generationgenius"). fadeOut();
+    $("#readmore-generationgenius"). fadeIn();
+    });        
+
+    $("#readmore-loom").click(function() {
+    $("#loom"). fadeIn();
+    $("#readmore-loom"). fadeOut();
+    });
+    $("#readless-loom").click(function() {
+    $("#loom"). fadeOut();
+    $("#readmore-loom"). fadeIn();
+    });
+
 });
